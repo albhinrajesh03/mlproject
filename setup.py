3bd7bf6452f6175d4d3864eb21ca141f3714adce
albhinrajesh03 <albhinrajesh03@gmail.com>
2024-03-10 11:30:26 +0530

    Second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name='mlproject',
+    version='0.0.1',
+    author='Albhin Rajesh',
+    author_email='albhinrajesh03@gmail.com',
+    packages=find_packages(),
+    install_requirs=['pandas','numpy','seaborn']
+)
